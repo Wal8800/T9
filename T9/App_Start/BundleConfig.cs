031384b5f6a487ece5c5848e5644b79b18583f62
Chang Kon Han <chang_kon@hotmail.com>
2015-08-09 08:31:01 +1200

    Added filter function for parking. Added basic bus locations. Not sure it works properly.

--- T9/App_Start/BundleConfig.cs
+++ T9/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace T9
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
@@ -41,6 +42,9 @@ namespace T9
 
             // Load athop test api
             bundles.Add(new ScriptBundle("~/Scripts/TestAtHop").Include("~/Scripts/TestAtHop.js"));
+
+            // Load stylesheets
+            //bundles.Add(new StyleBundle("~/Content/themes/site_global").Include("~/Content/themes/site_global.css"));
         }
     }
 }
--- T9/Scripts/TestAtHop.js
+++ T9/Scripts/TestAtHop.js
@@ -1,12 +1,15 @@
 ﻿//console.log("hello");
 //lat: -36.8498
 //long: 174.7650
-
+//roskill
 var lat = "";
 var lng = "";
 var apiKey = "833ef094-eb58-4f6e-812f-ae9b37b45719";
-var urllocation = "Queen Street";
 var parkingLocations = "";
+var routeLocations = [];
+var startStops = [];
+var endStops = [];
+var trips = [];
 
 (function () {
     if (navigator.geolocation) {
@@ -91,7 +94,7 @@ $("#atButton").on("click", function () {
     $.when(
 
         jQuery.ajax({
-            url: "https://api.at.govt.nz/v1/public/display/parkinglocations?callback=" + urllocation.replace(" ", "%20") + "&api_key=" + apiKey,
+            url: "https://api.at.govt.nz/v1/public/display/parkinglocations?callback=" + $("#query").val().replace(" ", "%20") + "&api_key=" + apiKey,
             type: "GET",
 
             contentType: 'application/json; charset=utf-8',
@@ -105,10 +108,9 @@ $("#atButton").on("click", function () {
                 var result = resultData;
                 var strlength = result.length;
                 var strindex = result.indexOf("{\"status\":");
-                var substr = result.substring(strindex, strlength-2);
+                var substr = result.substring(strindex, strlength - 2);
                 var jsonObject = JSON.parse(substr);
                 parkingLocations = jsonObject.response;
-                console.log(jsonObject);
                 //console.log(resultData);
                 //console.log(result);
             },
@@ -119,6 +121,161 @@ $("#atButton").on("click", function () {
     ).then(function () {
         debugger;
         console.log(parkingLocations);
+        parkingLocations = parkingLocations.filter(function (value) {
+            if (value.id <= 20) {
+                return true;
+            } else {
+                return false;
+            }
+        });
+        console.log(parkingLocations);
     });
 
+});
+
+$("#busButton").on("click", function () {
+    $.when(
+        jQuery.ajax({
+            url: "https://api.at.govt.nz/v1/gtfs/routes/search/" + "newmarket" + "?api_key=" + apiKey,
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function (resultData) {
+                //here is your json.
+                // process it
+
+                // return result
+                console.log("Routes by search");
+                //debugger;
+                var result = resultData.response;
+                result.forEach(function (element) {
+                    routeLocations.push(element.route_id);
+                });
+                console.log(routeLocations);
+                //console.log(resultData);
+                //console.log(result);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("fail");
+            }
+        }),
+        jQuery.ajax({
+            url: "https://api.at.govt.nz/v1/gtfs/stops/search/" + "roskill" + "?api_key=" + apiKey,
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function (resultData) {
+                //here is your json.
+                // process it
+
+                // return result
+                console.log("Stops by search");
+                //debugger;
+                var result = resultData.response;
+                result.forEach(function (element) {
+                    startStops.push(element.stop_id);
+                });
+                console.log(startStops);
+                //console.log(resultData);
+                //console.log(result);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("fail");
+            }
+        }),
+        jQuery.ajax({
+            url: "https://api.at.govt.nz/v1/gtfs/stops/search/" + "newmarket" + "?api_key=" + apiKey,
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function (resultData) {
+                //here is your json.
+                // process it
+
+                // return result
+                console.log("Stops by search");
+                //debugger;
+                var result = resultData.response;
+                result.forEach(function (element) {
+                    endStops.push(element.stop_id);
+                });
+                console.log(endStops);
+                //console.log(resultData);
+                //console.log(result);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("fail");
+            }
+        })
+
+
+    ).then(function () {
+        $.when(
+            jQuery.ajax({
+                url: "https://api.at.govt.nz/v1/gtfs/trips/routeid/" + routeLocations[0] + "?api_key=" + apiKey,
+                type: "GET",
+
+                contentType: 'application/json; charset=utf-8',
+                success: function (resultData) {
+                    //here is your json.
+                    // process it
+
+                    // return result
+                    console.log("Trip of first routes");
+                    //debugger;
+                    var result = resultData.response;
+                    var seen = [];
+                    result.forEach(function (element) {
+                        if (seen.indexOf(element.trip_id) == -1) {
+                            trips.push(element.trip_id);
+                            seen.push(element.trip_id);
+                        }
+                    });
+                    console.log(trips);
+                    //console.log(resultData);
+                    //console.log(result);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log("fail");
+                }
+            })
+        ).then(function () {
+            $.when(
+                jQuery.ajax({
+                    url: "https://api.at.govt.nz/v1/gtfs/stopTimes/tripId/" + trips[0] + "?api_key=" + apiKey,
+                    type: "GET",
+
+                    contentType: 'application/json; charset=utf-8',
+                    success: function (resultData) {
+                        //here is your json.
+                        // process it
+
+                        // return result
+                        console.log("All stops in that trip");
+                        //debugger;
+                        var result = resultData.response;
+                        var stopIds = [];
+                        result.forEach(function (element) {
+                            stopIds.push(element.stop_id);
+                        });
+                        console.log(stopIds);
+
+                        stopIds.forEach(function (element) {
+                            if (startStops.indexOf(element) != -1 || endStops.indexOf(element) != -1) {
+                                console.log(element);
+                            }
+                        });
+
+                        //console.log(resultData);
+                        //console.log(result);
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        console.log("fail");
+                    }
+                })
+            ).then(function () {
+
+            });
+        });
+    });
 });
